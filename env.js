3c6cfa7573d6684e73ab5040e9acde290d5bde3a
Henrique Rieger <henrique.rieger@gmail.com>
2021-02-17 20:31:26 -0300

    init

---
+++ env.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv');
+
+const env = {
+  TEST: 'test',
+};
+
+module.exports = Object.freeze(env);
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Title</div>,
+  document.getElementById('application'),
+);
+
+module.hot.accept();
